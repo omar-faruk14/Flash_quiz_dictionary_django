8e99e4332df3d93379bd5184bb5445096be24e39
Omar Faruk <Omarfaruk885@gmail.com>
2023-06-18 23:13:38 +0600

    First Update

---
+++ QuizAndFlash/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for QuizAndFlash project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QuizAndFlash.settings')
+
+application = get_asgi_application()
