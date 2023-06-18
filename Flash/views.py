37a4d320ed61fad32ff75b0944777dacd53ab2c5
Omar Faruk <Omarfaruk885@gmail.com>
2023-06-19 01:33:49 +0600

    Make hidden 1 word

--- Flash/views.py
+++ Flash/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render, HttpResponse
 from .models import FlashWord
 
+from django.shortcuts import render
+from .models import FlashWord
+
 def flash_word(request):
-    flash_words = FlashWord.objects.order_by('?')[:1]  # Retrieve 5 random flash words
+    if request.method == 'GET':
+        flash_words = FlashWord.objects.order_by('?')[:1]  # Retrieve 1 random flash word
 
-    context = {
-        'flash_words': flash_words
-    }
+        context = {
+            'flash_word': flash_words[0],
+            'show_full_word': False,
+        }
 
-    return render(request, 'flash_word.html', context)
+        return render(request, 'flash_word.html', context)
+
+    elif request.method == 'POST':
+        flash_words = FlashWord.objects.order_by('?')[:1]  # Retrieve 1 random flash word
+
+        context = {
+            'flash_word': flash_words[0],
+            'show_full_word': True,
+        }
+
+        return render(request, 'flash_word.html', context)
+
 
 def home(request):
     return render(request,'index.html')
