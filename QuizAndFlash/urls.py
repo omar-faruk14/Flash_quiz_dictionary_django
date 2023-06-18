ac50c4526c584a1f15a727c633c5559b4b7d3a42
Omar Faruk <Omarfaruk885@gmail.com>
2023-06-18 23:20:40 +0600

    Optimize Urls.py:

--- QuizAndFlash/urls.py
+++ QuizAndFlash/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from Flash import views as Flash_View
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    #Flash app Route
     path('',Flash_View.home,name="Home")
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
